53763798247ed4c6df5e26afe531321feabfbd4c
Binisor Maria <lavinia.binisor@gmail.com>
2023-06-12 13:29:42 +0300

    this is the initial commit for the Cupcake.

---
+++ CupcakeCorner_12.06/CupcakeCorner_12_06App.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCorner_12_06App.swift
+//  CupcakeCorner_12.06
+//
+//  Created by ionut.binisor on 12.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCorner_12_06App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
