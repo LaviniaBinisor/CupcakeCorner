1b86eb8f538dd197a7209a52a87a50ba084bffdd
Binisor Maria <lavinia.binisor@gmail.com>
2023-06-14 09:15:54 +0300

    testing a commit

--- CupcakeCorner_12.06/ContentView.swift
+++ CupcakeCorner_12.06/ContentView.swift
@@ -34,6 +34,7 @@ struct ContentView: View {
             Text("Hello, world!")
                 .font(.title)
                 .foregroundColor(.green)
+            
         }
         .padding()
     }
